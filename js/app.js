5274539088caba46a56fbc1bad6bce88773b9a9d
Mariam <mariam.fouad94@gmail.com>
2018-12-31 23:17:45 +0300

    create the basic of the Player function

--- js/app.js
+++ js/app.js
@@ -32,13 +32,30 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function (){
+    this.sprite = 'images/char-horn-girl.png';
+    this.x=202;
+    this.y=373.5;
 
+}
 
+Player.prototype.update=function(){
+
+}
+
+Player.prototype.render=function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput=function(){
+
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
 const allEnemies = [new Enemy() , new Enemy()];
+const player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
